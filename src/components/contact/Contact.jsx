ac9454d941703b58c08864f98cf89936a794ceb3
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-03-15 10:03:26 +0900

    컴포넌트 폴더 생성 및 index.css 설정

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./contact.css";
+const Contact = () => {
+  return <div>Contact</div>;
+};
+
+export default Contact;
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./portfolio.css";
+const Portfolio = () => {
+  return <div>Portfolio</div>;
+};
+
+export default Portfolio;
