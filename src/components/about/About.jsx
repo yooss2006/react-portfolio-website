49b8015fe391d790b8e8e02da3dd74601b9409b9
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-03-15 11:48:51 +0900

    fix about  컴포넌트 완성 및 css 수정

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { BsAward } from "react-icons/bs";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <BsAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ years working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>clients</h5>
+              <small>200+ worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+            necessitatibus fugiat explicabo quas provident libero dicta,
+            voluptates dolorum eveniet dignissimos blanditiis, mollitia ut ipsum
+            quo vel natus. Aperiam, blanditiis voluptatum!
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
