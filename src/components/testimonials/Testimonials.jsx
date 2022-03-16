72f9e5be9b9fd3ccc7e4e5683a0aae3e18e6b907
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-03-16 13:22:45 +0900

    fix testimonials  컴포넌트 완성 및 css 수정

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,72 @@
 import React from "react";
 import "./testimonials.css";
+import Avt1 from "../../assets/avatar1.jpg";
+import Avt2 from "../../assets/avatar2.jpg";
+import Avt3 from "../../assets/avatar3.jpg";
+import Avt4 from "../../assets/avatar4.jpg";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: Avt1,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim architecto consectetur, ipsum animi officia. Fuga nemo vero pariatur aperiam.",
+  },
+  {
+    avatar: Avt2,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim architecto consectetur, ipsum animi officia. Fuga nemo vero pariatur aperiam.",
+  },
+  {
+    avatar: Avt3,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim architecto consectetur, ipsum animi officia. Fuga nemo vero pariatur aperiam.",
+  },
+  {
+    avatar: Avt4,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos enim architecto consectetur, ipsum animi officia. Fuga nemo vero pariatur aperiam.",
+  },
+];
+
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper
+        className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
